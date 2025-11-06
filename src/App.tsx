0bd0c641333eb2321604726591f5f6bc96e1ff20
DazaiMari <xunianhe332@gmail.com>
2025-11-06 17:01:31 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import SignIn from "./pages/AuthPages/SignIn";
+import SignUp from "./pages/AuthPages/SignUp";
+import NotFound from "./pages/OtherPage/NotFound";
+import UserProfiles from "./pages/UserProfiles";
+import AppLayout from "./layout/AppLayout";
+import LithoStudio from "./pages/LithoStudio/LithoStudio.tsx"
+import Collection from "./pages/Collection/Collection.tsx"
+import ChatRoom from "./pages/ChatRoom/ChatRoom.tsx"
+//other
+import Videos from "./pages/UiElements/Videos";
+import Images from "./pages/UiElements/Images";
+import Alerts from "./pages/UiElements/Alerts";
+import Badges from "./pages/UiElements/Badges";
+import Avatars from "./pages/UiElements/Avatars";
+import Buttons from "./pages/UiElements/Buttons";
+import LineChart from "./pages/Charts/LineChart";
+import BarChart from "./pages/Charts/BarChart";
+import Calendar from "./pages/Calendar";
+import BasicTables from "./pages/Tables/BasicTables";
+import FormElements from "./pages/Forms/FormElements";
+import Home from "./pages/Dashboard/Home";
+import Blank from "./pages/Blank";
+import { ScrollToTop } from "./components/common/ScrollToTop";
+
+
+export default function App() {
+  return (
+    <>
+      <Router>
+        <ScrollToTop />
+        <Routes>
+          {/* Dashboard Layout */}
+          <Route element={<AppLayout />}>
+            <Route index path="/" element={<LithoStudio />} />
+            <Route path="/LithoStudio" element={<LithoStudio />} />
+            <Route path="/Collection" element={<Collection />} />
+            <Route path="/ChatRoom" element={<ChatRoom />} />
+            <Route path="/profile" element={<UserProfiles />} />
+            <Route path="/blank" element={<Blank />} />
+
+            {/* Other */}
+            <Route path="/form-elements" element={<FormElements />} />
+            <Route path="/Calendar" element={<Calendar />} />
+            <Route path="/Home" element={<Home />} />
+            {/* Tables */}
+            <Route path="/basic-tables" element={<BasicTables />} />
+              {/*Ui Elements */}
+            <Route path="/alerts" element={<Alerts />} />
+            <Route path="/avatars" element={<Avatars />} />
+            <Route path="/badge" element={<Badges />} />
+            <Route path="/buttons" element={<Buttons />} />
+            <Route path="/images" element={<Images />} />
+            <Route path="/videos" element={<Videos />} />
+            {/* Charts */}
+            <Route path="/line-chart" element={<LineChart />} />
+            <Route path="/bar-chart" element={<BarChart />} />
+          </Route>
+
+          {/* Auth Layout */}
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+
+          {/* Fallback Route */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
---
+++ src/pages/LithoStudio/LithoStudio.tsx
@@ -0,0 +1,38 @@
+import PageBreadcrumb from "../../components/common/PageBreadCrumb";
+import PageMeta from "../../components/common/PageMeta";
+import DropzoneComponent from "../../components/form/form-elements/DropZone";
+import SelectInputs from "../../components/form/form-elements/SelectInputs";
+import TextAreaInput from "../../components/form/form-elements/TextAreaInput";
+import Button from "../../components/ui/button/Button.tsx";
+
+export default function LithoStudio() {
+  return (
+    <div>
+      <PageMeta
+       title="LithoGPT – Upload Stone Images & Generate AI Insights"
+       description="Upload your stone images and let LithoGPT reveal their natural beauty, structure, and meaning. Experience AI-powered stone appreciation combining art, geology, and Eastern aesthetics."
+      />
+      <PageBreadcrumb pageTitle="LithoStudio" />
+      <div className="min-h-screen rounded-2xl border border-gray-200 bg-white px-5 py-7 dark:border-gray-800 dark:bg-white/[0.03] xl:px-10 xl:py-12">
+        <div className="mx-auto w-full max-w-[630px] text-center space-y-6">
+          <h3 className="mb-4 font-semibold text-gray-800 text-theme-xl dark:text-white/90 sm:text-2xl">
+            Begin AI appreciation
+          </h3>
+          <p className="text-sm text-gray-500 dark:text-gray-400 sm:text-base">
+            Please upload a clear photo of your stone.
+            Try to capture its natural shape, texture, and color under soft light.
+            The AI will analyze the image and generate an interpretation for you.
+          </p>
+          <DropzoneComponent />
+          <TextAreaInput />
+          <SelectInputs />
+            <Button size="sm" variant="primary">
+              Button Text
+            </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
